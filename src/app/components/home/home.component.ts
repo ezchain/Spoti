e305bf1dc04d4a9f918fa6e921e4117661f00d88
ezchain <ezequielechain@gmail.com>
2019-03-14 23:57:31 -0300

    agrego cosas

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
 
 @Component({
   selector: 'app-home',
@@ -6,8 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styles: []
 })
 export class HomeComponent implements OnInit {
+paises: any[] = [];
+  constructor() { 
 
-  constructor() { }
+   
+  }
 
   ngOnInit() {
   }
